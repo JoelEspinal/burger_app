e026ccb405b395787fb4c0671ab12f4012cbbeb1
Joel Espinal <j.espinalcarrasco@gmail.com>
2020-02-06 23:26:48 +0000

    :construction: Add order screen

---
+++ lib/ui/order/order_screen.dart
@@ -0,0 +1,31 @@
+import 'package:burger_app/ui/ingredients/ingredients_sheet.dart';
+import 'package:burger_app/ui/order/order_content.dart';
+import 'package:flutter/material.dart';
+
+class OrderScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: false,
+      body: OrderBody(),
+      bottomSheet: Ingredientsheet(),
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 18.0),
+          child: RaisedButton(
+            color: Colors.yellow,
+            onPressed: () {},
+            child: const Text(
+              'Add to Cart',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
