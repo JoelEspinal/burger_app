99a4c7363817c2ca66b4293a7f8d17ca7ab28074
Joel Espinal <j.espinalcarrasco@gmail.com>
2020-02-07 22:39:40 +0000

    :lipstick:Format space between elements

--- lib/ui/ingredients/ingredients_widget.dart
+++ lib/ui/ingredients/ingredients_widget.dart
@@ -6,17 +6,39 @@ class IngredientWidget extends StatelessWidget {
     Size _screenSize = MediaQuery.of(context).size;
     double _height = _screenSize.height;
 
-    return Container(
-      height: _height,
-      child: Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Text("Vegetable"),
-            ],
-          ),
-          Text("Lista de ingredientes"),
-        ],
+    return Padding(
+      padding: EdgeInsets.only(
+        top: 18.0,
+        left: 18.0,
+        right: 18.0,
+      ),
+      child: Container(
+        height: _height,
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "Ingredients",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "\$0.05",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Text("Lista de ingredientes"),
+          ],
+        ),
       ),
     );
   }
