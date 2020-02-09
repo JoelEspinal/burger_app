b7ac2adf13e21b62835a02d6406b9298fb83b535
Joel Espinal <j.espinalcarrasco@gmail.com>
2020-02-09 13:16:56 +0000

    :clown_face:Add Ingredient bar

--- lib/ui/ingredients/ingredients_widget.dart
+++ lib/ui/ingredients/ingredients_widget.dart
@@ -3,6 +3,50 @@ import 'package:flutter/material.dart';
 class IngredientWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return null;
+    Size _screenSize = MediaQuery.of(context).size;
+    double _height = _screenSize.height;
+    return DefaultTabController(
+      length: 1,
+      child: DefaultTabController(
+        length: 3,
+        child: Container(
+          color: Colors.white,
+          child: Column(
+            children: <Widget>[
+              TabBar(
+                tabs: [
+                  Tab(
+                      icon: Icon(
+                    Icons.directions_car,
+                    color: Colors.black,
+                  )),
+                  Tab(
+                      icon: Icon(
+                    Icons.directions_transit,
+                    color: Colors.black,
+                  )),
+                  Tab(
+                      icon: Icon(
+                    Icons.directions_bike,
+                    color: Colors.black,
+                  )),
+                ],
+              ),
+              Container(
+                width: double.infinity,
+                height: _height * 0.5,
+                child: TabBarView(
+                  children: [
+                    Icon(Icons.directions_car),
+                    Icon(Icons.directions_transit),
+                    Icon(Icons.directions_bike),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
